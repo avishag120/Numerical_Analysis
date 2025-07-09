a99c345f7a69245e1390ae6dd906bbc001a77de6
avishag120 <avishel@ac.sce.ac.il>
2025-07-09 17:14:19 +0300

    Cleaned up bcolors class and added documentation

--- colors.py
+++ colors.py
@@ -1,15 +1,12 @@
 class bcolors:
+    """
+       Utility class for styling terminal text using ANSI escape codes.
+       Includes foreground colors, background colors, and formatting options
+       such as bold and underline for clearer output in CLI applications.
+       """
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[90m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
